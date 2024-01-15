7d935150fbeb10b7c34b59c6d9e91b8420a6d9c9
Rajibsigdel12345 <rajibsigdel12345@gmail.com>
2024-01-15 17:56:13 +0545

    Linear regression model implemented

---
+++ LinearRegression.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sympy import Predicate
+
+
+class LinearRegression():
+    def __init__(self, lr=0.1, n_iters=1000):
+        self.learn = lr
+        self.bias = None
+        self.weight = None
+        self.iteration = n_iters
+
+    def fit(self, X, y):
+        n_iter = self.iteration
+        self.bias = 0
+        n_samples, n_features = X.shape
+        self.weight = np.zeros(n_features)
+
+        for _ in range(n_iter):
+            predict = np.dot(X, self.weight) + self.bias
+            # Gradient descent gives the new adjustmet in weight parameter
+            dw = (1/n_samples)*np.dot(X.T, (predict-y))
+            # average of the error gives bias
+            db = (1/n_samples)*np.sum(predict-y)
+            self.weight = self.weight - self.learn*dw
+            self.bias = self.bias - self.learn*db
+
+    def predict(self, X):
+        predict = np.dot(X, self.weight)
+        return predict
